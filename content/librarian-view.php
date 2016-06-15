b11c697276b0d0b1c37d94a8894c10f631ca0ee2
PurwantoGz <purwanto@outlook.com>
2016-06-15 23:54:18 +0700

    Retrieve Data from database

--- content/librarian-view.php
+++ content/librarian-view.php
@@ -6,58 +6,33 @@
 <div class="container" id="tourpackages-carousel">
       <div class="row">
         <div class="profile">
-              <div class="col-md-4"><!--ONE PROFIL-->
+        <?php 
+        include_once 'config/base.php';
+        $db= new BaseClass();
+        $result=$db->displayTable('SELECT * FROM `mahad`.`employee`');
+        if ($result->num_rows>0) {
+          while ($row=$result->fetch_assoc()) {
+            echo '<div class="col-md-4"><!--ONE PROFIL-->
                 <div class="thumbnail wow fadeInUp animated" data-wow-delay=".1s" data-wow-animation-name="fadeInUp" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInUp;">
-                  <img src="" alt="" style="width: 200px;height: 200px; margin-top: 20px;">
+                  <img src="admin/assets/images/employee/'.$row["Foto"].'" alt="" style="width: 200px;height: 200px; margin-top: 20px;">
                   <div class="caption">
                     <h4 class="text-center">
-                      <strong>Murniana, S.Pd</strong>
+                      <strong>'.$row["EmployeeName"].'</strong>
                     </h4>
-                    <h6 class="text-center">Kepala Perpustakaan</h6>
+                    <h6 class="text-center">'.$row["Job"].'</h6>
                     <address>
-                        <p><i class="fa fa-home pr-10"></i>Address: Bodon, Jagalan, Banguntapan, Bantul</p>
-                        <p><i class="fa fa-globe pr-10"></i>Webiste: www.mtsbanguntapan.sch.id </p>
-                        <p><i class="fa fa-phone pr-10"></i>Mobile : (123) 456-7890 </p>
-                        <p><i class="fa fa-envelope pr-10"></i>Email :   <a href="javascript:;">murniana@mtsbanguntapan.sch.id</a></p>
+                        <p><i class="fa fa-home pr-10"></i>Address:'.$row["Address"].'<br>
+                        <i class="fa fa-globe pr-10"></i>Webiste: www.mtsbanguntapan.sch.id <br>
+                        <i class="fa fa-phone pr-10"></i>Mobile : (+62) '.$row["NoHandphone"].' <br>
+                        <i class="fa fa-envelope pr-10"></i>Email :   <a href="javascript:;">'.$row["EmployeeName"].'@mtsbanguntapan.sch.id</a></p>
                     </address>
                   </div>
                 </div>
-              </div><!--END ONE PROFIL-->
-              <div class="col-md-4"><!--ONE PROFIL-->
-                <div class="thumbnail wow fadeInUp animated" data-wow-delay=".1s" data-wow-animation-name="fadeInUp" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInUp;">
-                  <img src="" alt="" style="width: 200px;height: 200px; margin-top: 20px;">
-                  <div class="caption">
-                    <h4 class="text-center">
-                      <strong>Vasthi Kurniawan Jati</strong>
-                    </h4>
-                    <h6 class="text-center">Sirkulasi</h6>
-                    <address>
-                        <p><i class="fa fa-home pr-10"></i>Address: Bodon, Jagalan, Banguntapan, Bantul</p>
-                        <p><i class="fa fa-globe pr-10"></i>Webiste: www.mtsbanguntapan.sch.id </p>
-                        <p><i class="fa fa-phone pr-10"></i>Mobile : (123) 456-7890 </p>
-                        <p><i class="fa fa-envelope pr-10"></i>Email :   <a href="javascript:;">murniana@mtsbanguntapan.sch.id</a></p>
-                    </address>
-                  </div>
-                </div>
-              </div><!--END ONE PROFIL-->
-              <div class="col-md-4"><!--ONE PROFIL-->
-                <div class="thumbnail wow fadeInUp animated" data-wow-delay=".1s" data-wow-animation-name="fadeInUp" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInUp;">
-                  <img src="" alt="" style="width: 200px;height: 200px; margin-top: 20px;">
-                  <div class="caption">
-                    <h4 class="text-center">
-                      <strong>Sugiyanto</strong>
-                    </h4>
-                    <h6 class="text-center">Inventaris Perpustakaan</h6>
-                    <address>
-                        <p><i class="fa fa-home pr-10"></i>Address: Bodon, Jagalan, Banguntapan, Bantul</p>
-                        <p><i class="fa fa-globe pr-10"></i>Webiste: www.mtsbanguntapan.sch.id </p>
-                        <p><i class="fa fa-phone pr-10"></i>Mobile : (123) 456-7890 </p>
-                        <p><i class="fa fa-envelope pr-10"></i>Email :   <a href="javascript:;">murniana@mtsbanguntapan.sch.id</a></p>
-                    </address>
-                  </div>
-                </div>
-              </div><!--END ONE PROFIL-->
-
+              </div>';
+          }
+        }
+         ?>
+              <!--END ONE PROFIL-->
         </div>
         <!-- End row -->
       </div>
