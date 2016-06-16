d0c73471ca920130f3c117820421025e5f4d7d33
PurwantoGz <purwanto@outlook.com>
2016-06-16 08:38:59 +0700

    change css awesome

--- admin/admin.php
+++ admin/admin.php
@@ -13,9 +13,13 @@ session_start();
   <!-- Bootstrap 3.3.2 -->
     <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />    
     <!-- FontAwesome 4.3.0 -->
+    
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-    <!-- Ionicons 2.0.0 -->
-    <link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />    
+    
+    <link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />
+    
+    <link rel="stylesheet" href="assets/awesome/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/awesome/ionicons.min.css">    
     <!-- DATA TABLES -->
     <link href="assets/plugins/datatables/dataTables.bootstrap.css" rel="stylesheet" type="text/css" />
     <!-- Theme style -->
